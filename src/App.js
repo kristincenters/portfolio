1605066e1e80aea6a9f7d4b0278610a9c6236820
kcenters <kristincenters@gmail.com>
2020-06-26 19:04:20 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+// import Header from './components/Header';
+// import Section from './components/Section/';
+// import Card from './components/Card/';
+// import Footer from './components/Footer';
+
+function App() {
+	return (
+		<div>
+			App
+			{/* <Header />
+			<Section />
+			<Card />
+			<Footer /> */}
+		</div>
+	);
+}
+
+export default App;
