90b49b8dfd11db6c386715e40b0d417f7fe275aa
kcenters <kristincenters@gmail.com>
2020-06-28 19:20:43 -0500

    added readme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import sqlicon from '../assets/mysql.svg';
-//import mongoicon from '../assets/mongodb.png';
-// import file from '../assets/CentersResume.pdf';
 
 function Footer() {
 	return (
